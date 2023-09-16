64b115438ca5afcbc865b22b8a7d0afc1be1764b
mohamed <mg.samadony@ygmail.com>
2023-09-16 19:41:37 +0100

    first

---
+++ src/app/app.module.js
@@ -0,0 +1,90 @@
+"use strict";
+var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
+    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
+    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
+    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
+    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
+    var _, done = false;
+    for (var i = decorators.length - 1; i >= 0; i--) {
+        var context = {};
+        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
+        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
+        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
+        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
+        if (kind === "accessor") {
+            if (result === void 0) continue;
+            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
+            if (_ = accept(result.get)) descriptor.get = _;
+            if (_ = accept(result.set)) descriptor.set = _;
+            if (_ = accept(result.init)) initializers.unshift(_);
+        }
+        else if (_ = accept(result)) {
+            if (kind === "field") initializers.unshift(_);
+            else descriptor[key] = _;
+        }
+    }
+    if (target) Object.defineProperty(target, contextIn.name, descriptor);
+    done = true;
+};
+var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
+    var useValue = arguments.length > 2;
+    for (var i = 0; i < initializers.length; i++) {
+        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
+    }
+    return useValue ? value : void 0;
+};
+var __setFunctionName = (this && this.__setFunctionName) || function (f, name, prefix) {
+    if (typeof name === "symbol") name = name.description ? "[".concat(name.description, "]") : "";
+    return Object.defineProperty(f, "name", { configurable: true, value: prefix ? "".concat(prefix, " ", name) : name });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var app_routing_module_1 = require("./app-routing.module");
+var app_component_1 = require("./app.component");
+var navbar_component_1 = require("./navbar/navbar.component");
+var footer_component_1 = require("./footer/footer.component");
+var home_component_1 = require("./home/home.component");
+var about_component_1 = require("./about/about.component");
+var portfolio_component_1 = require("./portfolio/portfolio.component");
+var contact_component_1 = require("./contact/contact.component");
+var notfound_component_1 = require("./notfound/notfound.component");
+var AppModule = function () {
+    var _classDecorators = [(0, core_1.NgModule)({
+            declarations: [
+                app_component_1.AppComponent,
+                navbar_component_1.NavbarComponent,
+                footer_component_1.FooterComponent,
+                home_component_1.HomeComponent,
+                about_component_1.AboutComponent,
+                portfolio_component_1.PortfolioComponent,
+                contact_component_1.ContactComponent,
+                notfound_component_1.NotfoundComponent
+            ],
+            imports: [
+                platform_browser_1.BrowserModule,
+                app_routing_module_1.AppRoutingModule
+            ],
+            providers: [],
+            bootstrap: [app_component_1.AppComponent]
+        })];
+    var _classDescriptor;
+    var _classExtraInitializers = [];
+    var _classThis;
+    var AppModule = _classThis = /** @class */ (function () {
+        function AppModule_1() {
+        }
+        return AppModule_1;
+    }());
+    __setFunctionName(_classThis, "AppModule");
+    (function () {
+        var _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+        __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name, metadata: _metadata }, null, _classExtraInitializers);
+        AppModule = _classThis = _classDescriptor.value;
+        if (_metadata) Object.defineProperty(_classThis, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+        __runInitializers(_classThis, _classExtraInitializers);
+    })();
+    return AppModule = _classThis;
+}();
+exports.AppModule = AppModule;
